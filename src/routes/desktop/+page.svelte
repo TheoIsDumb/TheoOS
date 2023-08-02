5bbc841792f2ba1800f57097a33c58e513f593d8
theo <lesgooo1993@proton.me>
2023-08-02 10:19:21 +0530

    where ya goin

--- src/routes/desktop/+page.svelte
+++ src/routes/desktop/+page.svelte
@@ -2,21 +2,36 @@
   import { openedApps } from "$lib/store.js";
   import Panel from "$lib/components/Panel.svelte";
   import Dock from "$lib/components/Dock.svelte";
-  import { scale } from "svelte/transition";
+  import { scale, fade } from "svelte/transition";
+
+  let outOfFocus = false;
+
+  function enter() {
+    outOfFocus = false;
+  }
+  function leave() {
+    outOfFocus = true;
+  }
 </script>
 
-<div transition:scale>
+<div class="parent" transition:scale on:mouseenter={enter} on:mouseleave={leave}>
   <Panel />
 
   {#each $openedApps as openedApp}
     <svelte:component this={openedApp.app} />
   {/each}
+
+  {#if outOfFocus}
+    <div transition:fade class="fullscreen shutdown_blur flex flex-jc flex-ac">
+      <h1>where<br>ya<br>goin?</h1>
+    </div>
+  {/if}
 </div>
 
 <Dock />
 
 <style>
-  div {
+  div.parent {
     width: 100dvw;
     height: 100dvh;
     position: relative;
