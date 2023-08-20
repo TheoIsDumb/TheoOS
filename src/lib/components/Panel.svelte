4afe274b5850bcdec54b33395c8c7d55dc0a341a
theo <lesgooo1993@proton.me>
2023-08-21 01:20:52 +0530

    dock and panel: colors

--- src/lib/components/Panel.svelte
+++ src/lib/components/Panel.svelte
@@ -27,7 +27,7 @@
 <div class="panel flex items-center justify-between xl:flex-col
     h-8 w-[100dvw] xl:w-10 xl:h-[calc(100dvh-2rem)] px-4 xl:py-4 xl:ml-3
     fixed top-0 xl:top-1/2 xl:-translate-y-1/2
-    backdrop-blur bg-[#100f0f61] xl:bg-[#000000a3] xl:rounded-lg text-white"
+    backdrop-blur bg-[#000000a3] xl:rounded-lg text-white"
     in:fly={{ y: -50, delay: 300 }}>
         <button class="transition cursor-pointer"
             on:click={() => {
