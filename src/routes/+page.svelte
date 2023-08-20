3149c0cb9dd539cb3594c842c92fefe643cb9bca
theo <lesgooo1993@proton.me>
2023-08-21 02:25:23 +0530

    main page tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,11 @@
     } 
 </script>
 
-<div class="fixed top-0 left-0 w-[100dvw] h-[100dvh] gap-6 z-[10] bg-zinc-950 text-white flex flex-col justify-center items-center">
+<div class="main fixed top-0 left-0 w-[100dvw] h-[100dvh] bg-zinc-950 text-white">
+    <div class="test flex flex-col justify-center items-center gap-6 h-[inherit] w-[inherit] bg-[#000000b0]">
         <h1 class="text-3xl font-bold">what's your name?</h1>
         <input type="text"
-        class="p-2 bg-transparent border-2 border-white rounded [outline:0] text-white focus:border-blue-400 transition"
+        class="p-2 bg-transparent border-2 border-white rounded text-center [outline:0] text-white focus:border-blue-500 transition"
         bind:value={$userName}
         on:keypress={jao}
         autocomplete="off">
@@ -21,4 +22,9 @@
             class="px-6 py-1 rounded text-2xl transition hover:bg-blue-400
             {$userName ? 'bg-blue-500' : 'pointer-events-none bg-zinc-700'}">
         Go!</a>
+    </div>
 </div>
+
+<style>
+  div.main { background: url("/wall/image.avif") fixed center / cover; }
+</style>
