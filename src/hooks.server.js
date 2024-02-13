25881b7b922d6d54c5bec4a3cc406e2b9d16e19b
theo <lesgooo1993@proton.me>
2024-02-13 14:24:54 +0530

    won't let /desktop load if  is not present (sveltekit hooks)

---
+++ src/hooks.server.js
@@ -0,0 +1,14 @@
+import { userName } from "$lib/store"
+import { get } from "svelte/store"
+import { redirect } from "@sveltejs/kit"
+
+export async function handle({ event, resolve }) {
+	if (event.url.pathname.startsWith('/desktop')) {
+        if (get(userName) === "") {
+            throw redirect(303, '/')
+        }
+	}
+
+	const response = await resolve(event);
+	return response;
+}
