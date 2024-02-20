8d71ca02d6756327e3976f34e083b739752e0f7c
theo <lesgooo1993@proton.me>
2024-02-20 15:16:18 +0530

    context menu on desktop

---
+++ src/lib/components/ContextMenu.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { scale } from 'svelte/transition'
+
+    export let m = { x: 0, y: 0 }
+
+	const options = ["Cut", "Copy", "Paste", "Refresh"]
+</script>
+
+<div transition:scale={{ duration: 200 }}
+class="dialog h-auto w-40 border border-neutral-600 bg-neutral-950 text-neutral-200 rounded-md shadow-md fixed z-10 p-2"
+style="left: {m.x}px; top: {m.y}px">
+    {#each options as opt}
+        <div class="border-b border-neutral-700 p-1 last:border-b-0">{opt}</div>
+    {/each}
+</div>
--- src/routes/desktop/+page.svelte
+++ src/routes/desktop/+page.svelte
@@ -1,17 +1,43 @@
 <script>
+  import { scale } from "svelte/transition"
   import { openedApps } from "$lib/store.js"
   import Panel from "$lib/components/Panel.svelte"
   import Dock from "$lib/components/Dock.svelte"
-  import { scale } from "svelte/transition"
+  import ContextMenu from "$lib/components/ContextMenu.svelte"
+
+	let m = { x: 0, y: 0 }
+	
+	let rightClicked = false
+	
+	function handleContextMenu(event) {
+		rightClicked = true
+		
+		m.x = event.clientX;
+		m.y = event.clientY;
+		console.log(event.clientX, event.clientY);
+	}
+	
+	function disable(e) {
+		if (e.which === 1) {
+		rightClicked = false
+		}
+	}
 </script>
 
-<div class="parent w-[100dvw] h-[100dvh] relative overflow-hidden" transition:scale>
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="parent w-[100dvw] h-[100dvh] relative overflow-hidden" transition:scale
+on:contextmenu|preventDefault|self={handleContextMenu} on:click|self={disable}>
   <Panel />
 
   {#each $openedApps as openedApp}
     <svelte:component this={openedApp.app} />
   {/each}
 </div>
+
+{#if rightClicked}
+  <ContextMenu {m} />
+{/if}
 
 <Dock />
 
